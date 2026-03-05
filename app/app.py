05652afdc882ace9920e4bc22a5d37fac53a5d53
naadirlab <sharifnaadir@gmail.com>
2026-03-06 00:05:20 +0100

    python app

---
+++ app/app.py
@@ -0,0 +1,12 @@
+# app.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def first_pipeline():
+    return f'Welcome to my first CI/CD Pipeline!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
