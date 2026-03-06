0eb00a323680b86a32c860ea03e48c12e50dcc40
naadirlab <sharifnaadir@gmail.com>
2026-03-06 11:06:00 +0100

    syntax correction

--- app/app.py
+++ app/app.py
@@ -3,10 +3,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route('/')
 def first_pipeline():
-    return f'Welcome to my first CI/CD Pipeline!'
+    return 'Welcome to my first CI/CD Pipeline!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5002)  
